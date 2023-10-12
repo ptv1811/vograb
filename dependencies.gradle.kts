ef3f786e3e01526f24a40714a4db911abac6ac6e
ptv1811 <ltvan@apcs.vn>
2023-10-13 00:00:08 +0700

    Update version for kotlin, hilt core and spotless

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,7 +1,10 @@
 mapOf(
         Pair("minSdk", 30),
         Pair("compileSdk", 33),
-        Pair("gradleBuildTool",  "8.1.1")
+        Pair("gradleBuildTool",  "8.1.1"),
+        Pair("kotlin",  "1.9.0"),
+        Pair("hiltCoreVersion", "2.46"),
+        Pair("spotlessGradle", "6.1.0")
 ).entries.forEach {
         project.extra.set(it.key, it.value)
 }
